66c4c17ac921f528b34ec3d9376645058c7480e0
Npham23 <82294334+Npham23@users.noreply.github.com>
2021-10-12 17:51:53 -0400

    Initial commit

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //========= Setting Variables =============
+
+    
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
