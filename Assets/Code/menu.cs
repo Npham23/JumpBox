064881d179b7f2ce8752fab03f371225d02f9f2f
Npham23 <82294334+Npham23@users.noreply.github.com>
2021-10-21 15:07:33 -0400

    Jump Fox V.3

--- Assets/Code/menu.cs
+++ Assets/Code/menu.cs
@@ -6,12 +6,34 @@ using UnityEngine.SceneManagement;
 public class menu : MonoBehaviour
 {
     // this is all base on order in build setting, moving one scene to another
-    public void playLevel1()
+    public void playLevel1() // First Player level (1), should be the easiest level compared to the other
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void backToMainMenu()
+    public void playLevel2() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void playLevel3() // Player Level 3
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void backToMainMenu1() // For Level 1 to go back to main menu
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+
+    public void backToMainMenu2() // For Level 2 to go back to main menu
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    // for only Level 3 to quit to main menu
+    public void backToMainMenu3() // For Level 3 to go back to main menu
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
 }
