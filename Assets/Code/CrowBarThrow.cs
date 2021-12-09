6300b191427ead62b043101607a51a880891e3d9
Npham23 <82294334+Npham23@users.noreply.github.com>
2021-12-09 15:32:03 -0500

    Jump Box Ver 11.2

--- Assets/Code/CrowBarThrow.cs
+++ Assets/Code/CrowBarThrow.cs
@@ -5,25 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class CrowBarThrow : MonoBehaviour
 {
-    float speed = 5.0f;
+    GameObject player;
+    float speed = 5.0f; // speed of crowbar
     Rigidbody rb;
 
-    PlayerMovement target;
+    PlayerMovement target; // player is the target
     Vector2 moveDirection;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        target = GameObject.FindObjectOfType<PlayerMovement>();
-        moveDirection = (target.transform.position - transform.position).normalized * speed;
+        target = GameObject.FindObjectOfType<PlayerMovement>(); // find player
+        moveDirection = (target.transform.position - transform.position).normalized * speed; 
         rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
-        Destroy(gameObject, 10f);
+        Destroy(gameObject, 10f); // disappear if it never hits
     }
 
     void onTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name.Equals("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); // game over
         }
     }
 }
